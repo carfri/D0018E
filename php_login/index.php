c08566c7b40ed0c90870c16fa1e766977dc2a450
Jacob Eklund <jacob.eklund.ekl@gmail.com>
2017-11-12 15:08:10 +0100

    login works

---
+++ php_login/index.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+<html>
+<head>
+<title>Dank website</title>
+</head>
+<body>
+
+<?php
+if (!isset($_SESSION['id']))
+{
+	echo "<form action=login.php method='post'>
+	Username: <input type='text' name='username'><br>
+	Password: <input type='password' name='password'><br>
+	<input type='submit' value='login'>
+	</form>
+	";
+}
+else	
+{
+	echo "Hello " . $_SESSION['username'] . ".";
+	echo "<a href='logout.php'>Logout</a>";
+}
+?>
+
+</body>
+</html>
--- php_login/login.php
+++ php_login/login.php
@@ -4,6 +4,8 @@ $username = "jacekl-5";
 $password = "jacekl-5";
 $dbname = "jacekl5db";
 
+session_start();
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
@@ -14,14 +16,19 @@ if ($conn->connect_error) {
 $username = trim($_POST['username']);
 $password = trim($_POST['password']);
 
-$qry = 'Select id from Users where username="'.$username.'" and password="'.$password.'"';
+$qry = 'Select id,username from Users where username="'.$username.'" and password="'.$password.'"';
 $result = mysqli_query($conn,$qry);
 //print_r ($result);
 if(!$result || mysqli_num_rows($result) <= 0){
 	echo "Hey buddy, I think you've got the wrong door, the leather club's two blocks down.";
 }	else 	{
-echo "log in";
+	$row = mysqli_fetch_assoc($result);
+	$_SESSION['id'] = $row['id'];
+	$_SESSION['username'] = $row['username'];
+	header("Location: index.php");
+		
 }
 
 $conn->close();
+exit();
 ?>
