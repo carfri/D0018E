fa1d4b690bf4cc2ce76b176d8227702bd7a60c4e
Jacob Eklund <jacekl-5@A9737.its.ltu.se>
2017-11-30 15:29:17 +0100

    register fix

--- php_login/register.php
+++ php_login/register.php
@@ -1,6 +1,6 @@
 
 <?php
-	require('connect.php');
+    require('connect.php');
     require('registerform.php');
     // If the values are posted, insert them into the database.
     if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['city']) && isset($_POST['address'])){
@@ -12,7 +12,7 @@
         $address = $_POST['address'];
         echo 'got this far';
  
-        $sql = "INSERT INTO customers (email, password, firstname, lastname,shipment_name,shipment_address, shipment_city, isAdmin) VALUES ('$email', '$password', '$firstname', '$lastname','?' ,'$address', '$city', '0')";
+        $sql = "INSERT INTO customers (email, password, firstname, lastname,shipment_address, shipment_city, isAdmin) VALUES ('$email', '$password', '$firstname', '$lastname','$address', '$city', '0')";
         if($conn->query($sql) === TRUE){
             header('Location: index.php');
             echo "User Created Successfully.";
