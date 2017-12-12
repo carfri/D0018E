21b10d8ff08d610d0ea20e5201cbe1606c9f0089
Jacob Eklund <jacekl-5@A9737.its.ltu.se>
2017-12-12 18:25:10 +0100

    Final DEL 3

--- php_login/action/addc.php
+++ php_login/action/addc.php
@@ -8,15 +8,26 @@
 	$rating = trim($_POST['rating']);
     $qry1 = "SELECT customerID FROM comments WHERE productID = $id AND customerID =" . $_SESSION['id'];
     $result1 = mysqli_query($conn, $qry1);
-    if(!$result1 || mysqli_num_rows($result1) >= 1){
-        $qry1 = "UPDATE comments SET comment= '$comment', rating= '$rating' WHERE productID = $id AND customerID=" . $_SESSION['id'];
-        mysqli_query($conn, $qry1);
-        header('Location: ../product.php?id='.$id);
-    }else{
-	    $qry = "INSERT INTO comments (comment, customerID, productID, rating) VALUES ('$comment', '" . $_SESSION['id'] . "', '$id', '$rating')";
-	    $result = mysqli_query($conn,$qry);
-	    header('Location: ../product.php?id='.$id);
-    }
+
+	$qry = "select customerID from orders where statusATM = 'shiped' and ProductID = $id and customerID =". $_SESSION['id'];
+	$r = mysqli_query($conn, $qry);
+	if(mysqli_num_rows($r) > 0){
+
+		if(!$result1 || mysqli_num_rows($result1) >= 1){
+		    $qry1 = "UPDATE comments SET comment= '$comment', rating= '$rating' WHERE productID = $id AND customerID=" . $_SESSION['id'];
+		    mysqli_query($conn, $qry1);
+		    header('Location: ../product.php?id='.$id);
+		}else{
+			$qry = "INSERT INTO comments (comment, customerID, productID, rating) VALUES ('$comment', '" . $_SESSION['id'] . "', '$id', '$rating')";
+			$result = mysqli_query($conn,$qry);
+			header('Location: ../product.php?id='.$id);
+		}
+	}
+
+	else
+	{
+		echo "buy first";
+	}
 	$conn->close();
 	exit();
 ?>
