8bdcd8ca29f7fa7ab4adbeecdcdccd54d50808ef
Jacob Eklund <jacekl-5@A9737.its.ltu.se>
2017-12-12 18:09:26 +0100

    final2

--- php_login/action/placeOrder.php
+++ php_login/action/placeOrder.php
@@ -3,21 +3,21 @@
 
 	require('../connect.php');
 
-	$qry1 = "select sum(o.ammountOrdered*p.price) as total from orders o join  products p on o.productID = p.id where o.customerID = 3 and o.statusATM = 'cart'";
-	r1 = mysqli_query($conn, $qry1);
+	$qry1 = "select sum(price) as total from orders where customerID =" . $_SESSION['id'] . " and statusATM = 'cart'";
+	$r1 = mysqli_query($conn, $qry1);
 	$row = mysqli_fetch_array($r1);
-	$one = row['total'];
+	$one = $row['total'];
 
-	$qry2 = "select sum(o.ammountOrdered*p.price) as total from orders o join  products p on o.productID = p.id where o.customerID = 3 and o.statusATM = 'cart'";
-	r2 = mysqli_query($conn, $qry2);
+	$qry2 = "select sum(o.ammountOrdered*p.price) as total from orders o join  products p on o.productID = p.id where o.customerID =" . $_SESSION['id'] . " and o.statusATM = 'cart'";
+	$r2 = mysqli_query($conn, $qry2);
 	$row = mysqli_fetch_array($r2);
-	$two = row['total'];
+	$two = $row['total'];
 
-	if($one == $tow)
+	if($one == $two)
 	{
 		$qry3 = "UPDATE orders SET statusATM ='bought' WHERE customerID=" . $_SESSION['id']. " and statusATM = 'cart'";
 		mysqli_query($conn, $qry3);
-		header('Location: ../userWelcome.php');
+		//header('Location: ../userWelcome.php');
 	}
 	else
 	{
