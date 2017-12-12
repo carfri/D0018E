fa6157825a81d0d7f1f2121a46f7f2f4618f5ff9
Jacob Eklund <jacekl-5@A9737.its.ltu.se>
2017-12-12 16:43:14 +0100

    amount

--- php_login/action/add_to_cart.php
+++ php_login/action/add_to_cart.php
@@ -5,16 +5,19 @@
     
 	$item = trim($_POST['b']);
     $price = trim($_POST['price']);
+	$amount = trim($_POST['amount']);
+
+
     $qry1 = "SELECT id FROM orders WHERE productID =" . $item . " AND customerID=" . $_SESSION['id'] . " AND statusATM = 'cart'";
     $result = mysqli_query($conn,$qry1);
 
     if(!$result || mysqli_num_rows($result) <= 0){
 	    $qry = 'INSERT INTO orders (customerID, productID,ammountOrdered, price, statusATM)
-	    VALUES ("'.$_SESSION['id'].'","'.$item.'","1","'.$price.'","cart")';
+	    VALUES ("'.$_SESSION['id'].'","'.$item.'","'.$amount.'","'.$price.'","cart")';
 	    mysqli_query($conn,$qry);
     }
     else{
-        $qry2 = "UPDATE orders SET ammountOrdered = ammountOrdered + 1, price = ammountOrdered *" . $price . " WHERE productID =" . $item . " AND customerID=" . $_SESSION['id'];
+        $qry2 = "UPDATE orders SET ammountOrdered = ammountOrdered + ".$amount.", price = ammountOrdered *" . $price . " WHERE productID =" . $item . " AND customerID=" . $_SESSION['id'];
         mysqli_query($conn, $qry2);
     }
     header('Location: ../userWelcome.php');
