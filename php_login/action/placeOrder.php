27f801e426edb608974e7c16a7686a2cdf84f4ae
Jacob Eklund <jacekl-5@A9737.its.ltu.se>
2017-12-12 18:05:38 +0100

    final

--- php_login/action/placeOrder.php
+++ php_login/action/placeOrder.php
@@ -2,7 +2,26 @@
 	session_start();
 
 	require('../connect.php');
-    $qry2 = "UPDATE orders SET statusATM ='bought' WHERE customerID=" . $_SESSION['id']. " and statusATM = 'cart'";
-    mysqli_query($conn, $qry2);
+
+	$qry1 = "select sum(o.ammountOrdered*p.price) as total from orders o join  products p on o.productID = p.id where o.customerID = 3 and o.statusATM = 'cart'";
+	r1 = mysqli_query($conn, $qry1);
+	$row = mysqli_fetch_array($r1);
+	$one = row['total'];
+
+	$qry2 = "select sum(o.ammountOrdered*p.price) as total from orders o join  products p on o.productID = p.id where o.customerID = 3 and o.statusATM = 'cart'";
+	r2 = mysqli_query($conn, $qry2);
+	$row = mysqli_fetch_array($r2);
+	$two = row['total'];
+
+	if($one == $tow)
+	{
+		$qry3 = "UPDATE orders SET statusATM ='bought' WHERE customerID=" . $_SESSION['id']. " and statusATM = 'cart'";
+		mysqli_query($conn, $qry3);
+		header('Location: ../userWelcome.php');
+	}
+	else
+	{
+		echo "price has changed";
+	}
     
 ?>
