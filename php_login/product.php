ee9ccb517523f7ef1e6cf4c7c3af55fd4ae59611
Jacob Eklund <jacob.eklund.ekl@gmail.com>
2017-12-12 01:39:03 +0100

    ship

--- php_login/product.php
+++ php_login/product.php
@@ -3,12 +3,18 @@
 	$product = $_GET['id'];
 	$query = "SELECT name, price, ininventory FROM products WHERE id = '" . $product . "'" ;
 	$query1 = "SELECT cus.email, c.comment, c.rating FROM comments c JOIN customers cus ON cus.id = c.customerID WHERE c.productID = " . $product;
+	$query2 = "select avg(rating) as avg from comments where productid = '$product'";
     $result = mysqli_query($conn, $query);
 	$result1 = mysqli_query($conn, $query1);	
+	$result2 = mysqli_query($conn, $query2);
 
 	$row = mysqli_fetch_array($result);
 	echo "<h1>" . $row['name'] ."</h1><br>| Price: " . $row['price'] . " | Inventory: " . $row['ininventory'] . " |" ; 
 
+	$row = mysqli_fetch_array($result2);
+	echo "avg rating : ";
+	echo $row['avg'];
+	
 	echo "<br>Add comment<br>";
 				echo "<form action=action/addc.php method='post'>
 				<input type='hidden' name='id'value=" . $product . ">
